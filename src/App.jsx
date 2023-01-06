88aaa6fb090b25eb2dec822a886153d89d9883cb
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-06 17:44:20 +0300

    feat: todoprovider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./components/Header";
-import Content from "./components/Content";
+import Content from "./components/Content/index";
 import Footer from "./components/Footer";
+import { ToDoProvider } from "./contexts/ToDoContext";
 
 function App() {
   return (
-    <>
+    <ToDoProvider>
       <section className="todoapp">
         <Header />
         <Content />
       </section>
       <Footer />
-    </>
+    </ToDoProvider>
   );
 }
 
